16cda99fdee5a026fa25dc0c4a7b0844eee5b9bc
Trostin Geles <trostinrocks@icloud.com>
2023-09-07 07:53:31 -0400

    fixing mistakes sorry jeff

--- ch02/ten_mathematical_expressions.cpp
+++ ch02/ten_mathematical_expressions.cpp
@@ -9,7 +9,7 @@ int main() {
     for (int i = 1; i <= 10; ++i){
         rn1 = rand() % 100;
         rn2 = rand() % 100;
-        product = rn1 + rn2;
+        product = rn1 * rn2;
 
         cout << i << period << " The product of " << rn1 << " and " << rn2 << " is " << product << '.' << endl;
         cout << " " << endl;
