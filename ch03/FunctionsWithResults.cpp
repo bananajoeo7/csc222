577b70f8c5f73bea0f6c7e33856c285fac6fd0f4
Trostin Geles <trostinrocks@icloud.com>
2023-09-07 09:47:05 -0400

    exercise 3

---
+++ ch03/FunctionsWithResults.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+/* Answer:
+1. If a returned value isn't used or assigned to a variable it will be ignored and forgotten.
+
+2. If the new_line() has an interger addded to it it will result in an error that says you 
+can't make an opperator to a binary expression.
+*/
+
+/*
+void new_line();
+
+int main() {
+    cout << "First Line." << endl;
+    new_line() + 7;
+    cout << "Second Line." << endl;
+    return 0;
+}
+
+void new_line() {
+    cout << endl;
+}
+*/
--- ch03/ParametersAndArguments.cpp
+++ ch03/ParametersAndArguments.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <math.h>
+#include <string>
 using namespace std;
 
 /* 
 Answer:
-Since the function calls for a char it ends up just removing the input all together 
-and it outputs a empty line.
+Since the function calls for a char and inputing some other type of variable ends 
+up just removing the input all together and it outputs a empty line or it won't compile.
 */
 
-void print_twice(string phil) {
+/*
+void print_twice(char phil) {
     cout << phil << phil << endl;
 }
 
@@ -16,11 +18,14 @@ int main() {
     char argument = 'b';
     int yipee = 7;
     float me_OOOW = 4.20;
+    string amongus = "SUS";
     
     print_twice('a');
     print_twice(argument);
     print_twice(yipee);
     print_twice(me_OOOW);
+    print_twice(amongus);
 
     return 0;
 }
+*/
