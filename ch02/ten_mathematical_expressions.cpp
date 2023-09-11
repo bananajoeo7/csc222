02e13da089d6c9678fc6478f7a75189754c6f9ae
Trostin Geles <trostinrocks@icloud.com>
2023-09-11 08:12:46 -0400

    TmE.cpp update

--- ch02/ten_mathematical_expressions.cpp
+++ ch02/ten_mathematical_expressions.cpp
@@ -2,16 +2,13 @@
 using namespace std;
 
 int main() {
-    char period;
-    int rn1, rn2, product;
-    period = '.';
-
+    
     for (int i = 1; i <= 10; ++i){
-        rn1 = rand() % 100;
-        rn2 = rand() % 100;
-        product = rn1 * rn2;
+        int rn1 = rand() % 100;
+        int rn2 = rand() % 100;
+        int product = rn1 * rn2;
 
-        cout << i << period << " The product of " << rn1 << " and " << rn2 << " is " << product << '.' << endl;
+        cout << i << "." << " The product of " << rn1 << " and " << rn2 << " is " << product << '.' << endl;
         cout << " " << endl;
     }
 
