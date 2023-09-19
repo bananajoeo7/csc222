a1b3d22920f8cabfcd486bc6a7123b84e65988d6
Trostin Geles <trostinrocks@icloud.com>
2023-09-19 08:33:19 -0400

    ch07 exercise 1

---
+++ ch07/hello_strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    string str1;
+    str1  = "Hello, ";
+    string str2 = "strings!";
+    cout << str1 << str2 << endl;
+    return 0;
+}
