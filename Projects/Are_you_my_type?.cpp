40430fd6d1bee45e57658e1ff0ac8336d3d3726b
Trostin Geles <trostinrocks@icloud.com>
2023-09-21 09:09:43 -0400

    Are you my type? skeleton code

--- Projects/Are_you_my_type?.cpp
+++ Projects/Are_you_my_type?.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 string sumADP(int num) {
-    // List goes here
+    // Vector goes here
     int sum = 0;
     string ADP = "";
     for(int i = 0; i = num; i++) {
         float division = num/i;
-        //check if division is a whole number/interger
-        // add division to list
+        // check if division is a whole number/interger
+        // add division to Vector
     }
 
-    for(int i = 0; i <= /*list.length()*/; i++) {
-        // iterate through list and add each number to sum
+    for(int i = 0; i <= /*Vector.length()*/; i++) {
+        // iterate through Vector and add each number to sum
     }
 
     if (sum > num) {
@@ -26,7 +26,7 @@ string sumADP(int num) {
     }
 
     cout << "Your number is " << ADP << endl;
-    cout << "The products of your number are " << /*print out list here*/ << endl;
+    cout << "The products of your number are " << /*print out Vector here*/ << endl;
     cout << "The sum of your products is " << sum << endl;
 }
 
