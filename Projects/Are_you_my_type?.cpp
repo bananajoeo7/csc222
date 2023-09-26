c8f36b3278ab42ab2799f63db2e06e513ea5152a
Trostin Geles <trostinrocks@icloud.com>
2023-09-26 08:10:13 -0400

    syntax updates

--- Projects/Are_you_my_type?.cpp
+++ Projects/Are_you_my_type?.cpp
@@ -8,41 +8,43 @@ void are_you_my_type(int num);
 int num;
 
 int main() {
-  while (cin >> num) {
-    are_you_my_type(num);
-  }
+    cout << "Enter a number: " << endl;
+    while (cin >> num) {
+        are_you_my_type(num);
+    }
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 void are_you_my_type(int num) {
-  vector<int> factors = {1};
-  int sum = 1;
+    vector<int> factors = {1};
+    int sum = 1;
 
-  cout << num << endl;
+    cout << endl;
+    cout << num << endl;
+    
+    for(int i = 2; i <= (num / 2); i++) {
+        if (num % i == 0 && i != num) {
+            factors.push_back(i);
 
-  for(int i = 2; i <= (num / 2); i++) {
-    if (num % i == 0 && i != num) {
-      factors.push_back(i);
-
-      sum += i;
+            sum += i;
+        }
     }
-  }
 
-  if (sum < num) {
-    cout << "Deficient";
-  } else if (sum == num) {
-    cout << "Perfect";
-  } else {
-    cout << "Abundant";
-  }
+    if (sum < num) {
+        cout << "Deficient";
+    } else if (sum == num) {
+        cout << "Perfect";
+    } else {
+        cout << "Abundant";
+    }
 
-  cout << " Factors are: ";
-  for (int &factor : factors) {
-    cout << factor << " ";
-  }
-  cout << endl;
+    cout << " Factors are: ";
+    for (int &factor : factors) {
+        cout << factor << " ";
+    }
+    cout << endl;
 
-  cout << "The sum of your factors is " << sum << endl << endl;
+    cout << "The sum of your factors is " << sum << endl << endl;
 }
 
