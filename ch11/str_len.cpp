4c95fccd846e00b6a4a319d0984cb46ebacc42ff
Trostin Geles <trostinrocks@icloud.com>
2023-10-04 08:49:14 -0400

    ch11 exercise 3

---
+++ ch11/str_len.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int str_len(char s[]){
+    int i = 0;
+    while (s[i] != '\0') i++;
+    return i;
+}
+
+int main() {
+    char string[] = "Will Caleb grow a beard when he is an adult \0";
+
+    cout << string << " is " << str_len(string) << " characters in length.\n";
+
+    return 0;
+}
