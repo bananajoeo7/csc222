55aa8619313b9abcc98fee9e1e1780982f2862fc
Trostin Geles <trostinrocks@icloud.com>
2023-10-04 20:38:19 -0400

    ch11 exercise 2

---
+++ ch11/shuffle.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <array>
+using namespace std;
+
+void printArray(int* array, int length) {
+    for (int i = 0; i < length; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+void shufflePure(int* array, int length) {
+    for (int i = 0; i < length; i++) {
+        int rand1 = 1+(rand() % 10);
+        int rand2 = 1+(rand() % 10);
+        int a = array[rand1];
+        int b = array[rand2];
+
+        array[rand1] = b;
+        array[rand2] = a;
+    }
+    cout << "Your pure shuffled array is: " << endl;
+    printArray(array, length);
+}
+
+void shuffleMod(int* array, int length) {
+    srand((int) time(0));
+    
+    for (int i = 0; i < length; i++) {
+        int rand1 = 1+(rand() % 10);
+        int rand2 = 1+(rand() % 10);
+        int a = array[rand1];
+        int b = array[rand2];
+
+        array[rand1] = b;
+        array[rand2] = a;
+    }
+    cout << "Your modified shuffled array is: " << endl;
+    printArray(array, length);
+}
+
+
+int main() {
+    int x[10] = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
+
+    cout << "Your array is: " << endl;
+    printArray(x, 10);
+    
+    shufflePure(x, 10);
+
+    shuffleMod(x, 10);
+
+}
--- ch11/str_len.cpp
+++ ch11/str_len.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int str_len(char s[]){
+int str_len(char s[]) {
     int i = 0;
     while (s[i] != '\0') i++;
     return i;
 }
 
 int main() {
-    char string[] = "Will Caleb grow a beard when he is an adult \0";
+    char string[] = "Will Caleb grow a beard when he is an adult? \0";
 
     cout << string << " is " << str_len(string) << " characters in length.\n";
 
