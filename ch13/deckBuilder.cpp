a0e86a4254a1e85bf8e57a35f352a22597f1e9a0
Trostin Geles <trostinrocks@icloud.com>
2023-10-26 08:37:43 -0400

    ch13 exercise 4

--- ch13/deckBuilder.cpp
+++ ch13/deckBuilder.cpp
@@ -30,6 +30,19 @@ string Card::to_string() const
     return rank_strings[rank] + " of " + suit_strings[suit];
 }
 
+vector<Card> build_deck() {
+    vector<Card> deck(52);
+    int i = 0;
+    for (int suit = 0; suit <= 3; suit++) {
+        for (int rank = 1; rank <= 13; rank++) {
+            deck[i].suit = suit;
+            deck[i].rank = rank;
+            i++;
+        }
+    }
+    return deck;
+}
+
 int main (){
-    
+    build_deck();
 }
