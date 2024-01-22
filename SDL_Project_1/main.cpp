d7d2e877658776e6fef66a33533d593d248bf3c4
Trostin Geles <trostinrocks@icloud.com>
2024-01-22 08:14:01 -0500

    trying to revert code

--- SDL_Project_1/main.cpp
+++ SDL_Project_1/main.cpp
@@ -1,51 +1,11 @@
 #include <iostream>
-#include <SDL2/SDL.h>
+#include <SDL2/include/SDL.h>
+#include <cmath>
 using namespace std;
 
-int main(int argc, char** argv)
-{
-    SDL_Window* window = nullptr;
-    SDL_Renderer* renderer = nullptr;
+const int SCREEN_WIDTH = 600;
+const int SCREEN_HEIGHT = 600;
+const int CELL_SIZE = 200;
 
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_CreateWindowAndRenderer(640*2, 480*1.5, 0, &window, &renderer);
-    SDL_RenderSetScale(renderer, 2, 2);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_Rect rect;
-    rect.w = 100;
-    rect.h = 100;
-    rect.y = 0;
-    rect.x = 0;
-
-    SDL_Rect rect2;
-    rect2.w = 100;
-    rect2.h = 100;
-    rect2.y = 50;
-    rect2.x = 50;
-
-    SDL_RenderDrawRect(renderer, &rect);
-    SDL_RenderFillRect(renderer, &rect);
-    SDL_RenderDrawRect(renderer, &rect2);
-    SDL_RenderFillRect(renderer, &rect2);
-
-    SDL_Rect intersection;
-
-    SDL_IntersectRect(&rect, &rect2, &intersection);
-
-    SDL_RenderDrawRect(renderer, &rect);
-    SDL_RenderDrawRect(renderer, &rect2);
-
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderFillRect(renderer, &intersection);
-
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderDrawPoint(renderer, 640/2, 480/2);
-
-    SDL_RenderPresent(renderer);
-    SDL_Delay(10000);
-
-    return 0;
-}
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
